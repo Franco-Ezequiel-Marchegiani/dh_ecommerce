99fe907aec670e28e606bf2ca5e8cb093bbb64fc
Franco Ezequiel <eze.cavs@hotmail.com>
2024-09-16 14:11:10 -0300

    Updating changes

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,18 +7,7 @@ const Home = () => {
 
   const [products, setProducts] = useState([])
 
-  const getProducts = async () =>{
-    try{
-      const response = await fetch('http://localhost:3000/products');
-      const data = await response.json()
-      setProducts(data)
-
-    }catch(error){
-      console.log(error);
-      throw(error)
-      
-    }
-  }
+  
   useEffect(() =>{
     getProducts();
   },[])
